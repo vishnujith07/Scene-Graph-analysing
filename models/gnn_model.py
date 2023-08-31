d94b314ec39280c96e5bd828407c0899b6fb4c9a
vishnujith07 <98251674+vishnujith07@users.noreply.github.com>
2023-08-31 22:11:11 +0530

    Create gnn_model.py

---
+++ models/gnn_model.py
@@ -0,0 +1,12 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class SceneGraphGNN(nn.Module):
+    def __init__(self, input_dim, hidden_dim, num_classes):
+        super(SceneGraphGNN, self).__init__()
+        # Define GNN layers and operations
+
+    def forward(self, graph_data):
+        # Implement the forward pass through GNN
+        return graph_data
