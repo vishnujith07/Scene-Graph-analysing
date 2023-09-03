ac5d707ce72b04e0189d636613df9db63d08ecb9
vishnujith07 <98251674+vishnujith07@users.noreply.github.com>
2023-09-03 20:43:51 +0530

    Update gnn_model.py

--- models/gnn_model.py
+++ models/gnn_model.py
@@ -1,12 +1,32 @@
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
 
-class SceneGraphGNN(nn.Module):
-    def __init__(self, input_dim, hidden_dim, num_classes):
-        super(SceneGraphGNN, self).__init__()
-        # Define GNN layers and operations
+# Step 1: Data Preparation (Assuming you have data in NumPy arrays)
+# X_scene_graph: Scene graph data (features)
+# X_common_sense: Common sense knowledge data (features)
+# y: Enhanced scene graph attributes (labels)
 
-    def forward(self, graph_data):
-        # Implement the forward pass through GNN
-        return graph_data
+# Combine scene graph and common sense data
+X_combined = np.concatenate((X_scene_graph, X_common_sense), axis=1)
+
+# Split the data into training, validation, and test sets
+X_train, X_temp, y_train, y_temp = train_test_split(X_combined, y, test_size=0.3, random_state=42)
+X_val, X_test, y_val, y_test = train_test_split(X_temp, y_temp, test_size=0.5, random_state=42)
+
+# Step 2: Feature Scaling (You may need more advanced preprocessing)
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_val_scaled = scaler.transform(X_val)
+X_test_scaled = scaler.transform(X_test)
+
+# Step 3: Model Training (Random Forest Classifier as an example)
+model = RandomForestClassifier(n_estimators=100, random_state=42)
+model.fit(X_train_scaled, y_train)
+
+# Step 4: Model Evaluation
+y_pred = model.predict(X_test_scaled)
+accuracy = accuracy_score(y_test, y_pred)
+print(f"Accuracy on the test set: {accuracy:.4f}")
